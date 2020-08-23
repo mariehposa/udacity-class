9a4f35bf35b49aa48e60957d667dee1f506c4d7f
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-23 19:50:36 +0100

    loop through array

--- quicksort.py
+++ quicksort.py
@@ -2,15 +2,22 @@
 Input a list.
 Output a sorted list."""
 def quicksort(array):
-    pivot = array[0]
 
     low = []
     high = []
     equal = []
     
     for i in array:
+        pivot = array[0]
         if(array[i] < pivot):
             low.append(array[i])
+        elif(array[i] > pivot):
+            high.append(array[i])
+        else:
+            equal.append(array[i])
+
+    # output = ort(low)+sort(equal)+sort(high)
+    return(low)
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
-print quicksort(test)
+print(quicksort(test))
