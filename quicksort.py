8b4cfcc6a692fbf91e8196a0dff454e0e72e8bc3
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-08-31 16:33:08 +0100

    remove redundant code

--- quicksort.py
+++ quicksort.py
@@ -24,25 +24,3 @@ def quicksort(array):
 
 test = [10, 7, 8, 9, 1, 5, 20]
 print(quicksort(test))
-
-# optimized version
-# """Implement quick sort in Python.
-# Input a list.
-# Output a sorted list."""
-
-# def quicksort(array):
-#     pivot_index = 0
-#     pivot = array[pivot_index]
-#     store_index = pivot_index + 1
-
-#     for x in range(pivot_index, len(array)):
-#         if array[x] < pivot:
-#             array[x], array[store_index] = array[store_index], array[x]
-    
-#     pivot = store_index
-#     store_index = pivot
-
-#     return array
-
-# test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
-# print(quicksort(test))
