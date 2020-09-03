fe332768d0bdcde0ecd900c0f58306f3cd55a1d7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-09-03 22:08:40 +0100

    add time complexity for the if statement

--- fibonnaci.py
+++ fibonnaci.py
@@ -1,9 +1,9 @@
 def get_fib(position):
     if position == 0 or position == 1:
-        return position
+        return position   #O(1)
     else:
         # print(position - 1, position - 2)
-        return(get_fib(position - 1) + get_fib(position - 2))
+        return(get_fib(position - 1) + get_fib(position - 2)) 
     return -1
 
 # Test cases
